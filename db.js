664ba3d651105d86b994e7f460883793746dcf4b
CnrGames <canirajuma@gmail.com>
2023-02-28 23:59:15 +0200

    Dynasty Start

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { MongoClient } = require('mongodb');
 let dbCon;
 /** "mongodb://localhost:27017/SibasTpm" */
+/*'mongodb+srv://isctem:isctem@isctemcluster.jwrxodo.mongodb.net/SIbasTpm?retryWrites=true&w=majority'*/
+
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect(
-      'mongodb+srv://isctem:isctem@isctemcluster.jwrxodo.mongodb.net/SIbasTpm?retryWrites=true&w=majority'
-    )
+    MongoClient.connect('mongodb://localhost:27017/SibasTpm')
       .then((client) => {
         dbCon = client.db();
         return cb();
