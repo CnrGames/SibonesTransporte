eb50479dfdb2a6eed1eb14fad0109b5ba64bd477
CnrGames <canirajuma@gmail.com>
2023-03-05 01:51:53 +0200

    after update2

--- db.js
+++ db.js
@@ -3,9 +3,11 @@ let dbCon;
 /** "mongodb://localhost:27017/SibasTpm" */
 /*'mongodb+srv://isctem:isctem@isctemcluster.jwrxodo.mongodb.net/SIbasTpm?retryWrites=true&w=majority'*/
 
+let a = 'mongodb+srv://isctem:isctem@isctemcluster.jwrxodo.mongodb.net/SIbasTpm?retryWrites=true&w=majority';
+let b = 'mongodb://localhost:27017/SibasTpm';
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect('mongodb://localhost:27017/SibasTpm')
+    MongoClient.connect(a)
       .then((client) => {
         dbCon = client.db();
         return cb();
